6f1deee114301c814a3d7765e3a77d4d02a04d71
Alison Wu <alison.wu23@gmail.com>
2023-05-03 17:03:22 +0800

    feat: build connection between server and client side

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,4 +1,12 @@
 import "./index.css";
+import { io } from "socket.io-client";
 import { name } from "@/utils";
 
 console.log("client side chatroom page", name);
+
+// 建立 node server 連接
+const clientInfo = io()
+clientInfo.on("join", (msg) => {
+  console.log("msg", msg);
+  
+})
