ad90e05fd9288ad11e1382c59a6655da461beca0
Alison Wu <alison.wu23@gmail.com>
2023-05-03 18:09:07 +0800

    feat: main page to chatroom page

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,8 +1,15 @@
 import "./index.css";
 import { io } from "socket.io-client";
-import { name } from "@/utils";
 
-console.log("client side chatroom page", name);
+const url = new URL(location.href);
+const userName = url.searchParams.get("user_name");
+const roomName = url.searchParams.get("room_name");
+
+if (!userName || !roomName) {
+  location.href = "/main/main.html";
+}
+
+console.log('chat', userName, roomName);
 
 // 建立 node server 連接
 const clientInfo = io()
